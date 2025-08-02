3b1665e77a204542dc59b2179a6e959cc300801f
Irfan-riyaz <133787187+Irfan-riyaz@users.noreply.github.com>
2025-08-03 01:22:30 +0530

    Update index.js

--- pages/api/department/index.js
+++ pages/api/department/index.js
@@ -3,16 +3,14 @@ import { connectToDatabase } from '../../../lib/mongodb';
 
 export default async function handler(req, res) {
   try {
-    const { db } = await connectToDatabase();
-    const departments = await db
-      .collection('department')
-      .find({})
-      .limit(5)
-      .toArray();
+    const client = await connectToDatabase();
+    const db = client.db();
+
+    const departments = await db.collection('department').find({}).toArray();
+
     res.status(200).json(departments);
   } catch (error) {
     console.error('API Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 }
-
